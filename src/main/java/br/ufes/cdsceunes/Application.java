5c73209b0b9f41a18ad140f0d16b4db34fd3219e
emethium <nikolas.serafini@gmail.com>
2017-08-17 10:53:56 -0300

    Delicious denied by CORS policy

--- src/main/java/br/ufes/cdsceunes/Application.java
+++ src/main/java/br/ufes/cdsceunes/Application.java
@@ -3,7 +3,11 @@ package br.ufes.cdsceunes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @EntityScan(basePackageClasses= { Application.class, Jsr310JpaConverters.class })
 @SpringBootApplication
@@ -12,4 +16,14 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurerAdapter corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/teachers").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
--- src/main/java/br/ufes/cdsceunes/controller/TeacherController.java
+++ src/main/java/br/ufes/cdsceunes/controller/TeacherController.java
@@ -2,6 +2,7 @@ package br.ufes.cdsceunes.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufes.cdsceunes.model.Teacher;
 import br.ufes.cdsceunes.repository.TeacherRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/teachers")
 @RestController
 public class TeacherController extends AbstractController<Teacher, TeacherRepository> {
